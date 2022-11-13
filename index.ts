052c180a07cae92520187d4567cba9730f613804
Eliot Lash <fadookie@gmail.com>
2022-11-12 18:04:20 -0800

    got account lookup and list lookup working but having an issue adding account to list

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello world")
+import assert from 'assert';
+
+const accessToken = require('./.access-token.json');
+import { login } from 'masto';
+
+async function main() {
+  const masto = await login({
+    url: 'https://social.coop',
+    accessToken,
+  });
+
+  const account = await masto.accounts.lookup({ acct: "Eliot_L@mastodon.social"});
+  assert.ok(account);
+
+  const lists = await masto.lists.fetchAll();
+
+  const list = await masto.lists.fetch(lists[0].id);
+
+  console.log("Got account:", account.acct,  " lists:", lists, " list:", list);
+
+  await masto.lists.addAccount(list.id, { accountIds: [account.id]});
+}
+
+main().catch((error) => {
+  console.error(error);
+});
