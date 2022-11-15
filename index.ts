753c047d63b56e3071d58b7a4305bcabd91d31aa
Eliot Lash <fadookie@gmail.com>
2022-11-14 22:55:25 -0800

    remove accounts that aren't being followed

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ async function main() {
 
     const rows: [string, string][] = [
       ["Test1", "ferrata@mastodon.cloud"],
+      ["Test1", "JoeSondow@tabletop.social"],
       ["Test2", "devonrubin@mastodon.art"],
       ["Test2", "alison@toot.site"],
       ["Test List", "Gargron@mastodon.social"],
@@ -28,7 +29,7 @@ async function main() {
     const preExistingLists = cloudListsInitial.filter(list => inputLists.includes(list.title));
     const newListNames = _.difference(inputLists, preExistingLists.map(list => list.title));
 
-    const newLists = await Promise.all(newListNames.map(async title => (
+    const newLists = await Promise.all(newListNames.map(async title => ( // TODO: Batch this
       await masto.lists.create({ title })
     )));
 
@@ -51,18 +52,30 @@ async function main() {
       }
     }
 
-    console.log(`Deduped account candidates, ${rows.length} accounts remain.`);
+    // Remove rows for accounts that the user isn't following
+    const accounts = await Promise.all(rows.map(async (row) => { // TODO: batch these requests
+      const account = await masto.accounts.lookup({ acct: row[1] }); // TODO: optimize by using cached account object if it was already in memory
+      assert.ok(account);
+      return account;
+    }));
+
+    const relationships = await masto.accounts.fetchRelationships(accounts.map(account => account.id));
+    for (const relationship of relationships) {
+      if (!relationship.following) {
+        const accountAddress = accounts.find(x => x.id === relationship.id)?.acct;
+        assert.ok(accountAddress);
+        _.remove(rows, row => row[1] === accountAddress);
+        console.log(`You are not following ${accountAddress}, removing this account.`);
+      }
+    }
+
+    console.log(`Cleaned account candidates, ${rows.length} accounts remain.`);
 
     const accountsByList = _.groupBy(rows, row => row[0]);
     console.log('accountsByList:', accountsByList); 
 
     for (const [listName, rowsForAccount] of Object.entries(accountsByList)) {
       // Look up account IDs of remaining accounts on the list
-      const accounts = await Promise.all(rowsForAccount.map(async (row) => { // TODO: batch these requests
-        const account = await masto.accounts.lookup({ acct: row[1] }); // TODO: optimize by using cached account object if it was already in memory
-        assert.ok(account);
-        return account;
-      }));
       const accountIds = accounts.map(account => account.id);
 
       console.log(`[${listName}] Got new accounts:`, accounts.map(a => a.acct), " accountIds:", accountIds, " lists:", cloudListsInitial);
