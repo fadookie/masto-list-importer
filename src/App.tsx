2b5c3a20a8623c63a6a7870d50b21f5f25fd7a1f
Eliot Lash <fadookie@gmail.com>
2022-11-15 08:19:43 -0800

    Add "fork me on github" banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'github-fork-ribbon-css/gh-fork-ribbon.css';
 import './App.css';
 import { Importer } from './Importer';
 
 function App() {
   return (
     <div className="App">
+      <a className="github-fork-ribbon" href="https://github.com/fadookie/masto-list-importer" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
       <Importer />
     </div>
   );
